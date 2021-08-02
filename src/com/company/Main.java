687015beb71025a44ac86da1ef8ead47b3dc585f
wooseok <diddmlvkf@gmail.com>
2021-08-02 20:26:33 +0900

    Update: 입력한 값의 유효성 검사 추가

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        for(int i = 1; i <= 9; i++){
-            System.out.println(number + "*" + i + "=" + number * i);
+        if (number < 2 || number > 9){
+            System.out.println("2이상, 9이하의 값만 입력할 수 있습니다.");
+        }else {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d * %d = %d\n", number, i, number * i);
+            }
         }
     }
 }

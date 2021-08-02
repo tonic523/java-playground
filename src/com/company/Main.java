9aa4838a0ff06dd4f114b7b2f5b412dae9822153
wooseok <diddmlvkf@gmail.com>
2021-08-02 22:25:29 +0900

    Update: 구구단 메소드 구현

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,30 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    public static int[] get_gugudan(int number) {
+        int[] result = new int[9];
+        if (number < 2 || number > 9){
+            return result;
+        }
+        for (int i = 1; i < 10; i++){
+            result[i-1] = number * i;
+        }
+        return result;
+    }
+
+    public static void print_gugudan(int[] gugudan){
+        for (int i = 0; i < 9; i++){
+            System.out.printf("%d ",gugudan[i]);
+        }
+    }
 
     public static void main(String[] args) {
         int[] numberGugudan = new int[9];
         for (int number = 2; number < 10; number++){
-            for (int i = 1; i < numberGugudan.length; i++){
-                numberGugudan[i] = number * i;
-            }
+            numberGugudan = get_gugudan(number);
             System.out.println();
             System.out.printf("%d단:",number);
-            for (int i = 1; i < numberGugudan.length; i++){
-                System.out.printf("%d ", numberGugudan[i]);
-            }
+            print_gugudan(numberGugudan);
         }
     }
 }

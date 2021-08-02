bdb65cb36500e0a9b2efda46aef98ad372c21d80
wooseok <diddmlvkf@gmail.com>
2021-08-02 18:37:32 +0900

    Update: 입력한 정수 9단까지 출력

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        System.out.println("number : " + number);
+        for(int i = 1; i <= 9; i++){
+            System.out.println(number + "*" + i + "=" + number * i);
+        }
     }
 }

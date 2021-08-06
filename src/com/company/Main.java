365eabec1433206275d245a810af45049d2262ab
wooseok <diddmlvkf@gmail.com>
2021-08-06 19:43:43 +0900

    Update: 종료 조건이 있는 반복 실행 구현

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calendar calendar = new Calendar();
-
-        System.out.println("반복 횟수를 입력하세요.");
-        int time = scanner.nextInt();
-        System.out.println("월을 입력하세요.");
-        int[] months = new int[time];
-
-        for (int i = 0; i < time; i++){
-            int month = scanner.nextInt();
-            months[i] = month;
-        }
-        for (int month : months){
+        int month;
+        while(true){
+            System.out.println("월을 입력하세요.");
+            month = scanner.nextInt();
+            if (month == -1){
+                System.out.println("Have a nice day!");
+                break;
+            }
+            scanner.nextLine();
             System.out.printf("%d월은 %d일까지 있습니다.\n",month,calendar.getDays(month));
         }
         scanner.close();

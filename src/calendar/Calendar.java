b1d17e79f33fd081ace0ef8eef3bb82c3c8072d5
wooseok <diddmlvkf@gmail.com>
2021-08-07 12:50:57 +0900

    Update: 시작 요일에 맞는 달력 출력

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,18 +1,40 @@
 package calendar;
 
+import java.util.Arrays;
+
 public class Calendar {
+
+    String[] WEEKDAYS = {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
     public Calendar(){}
 
-    public int isLeapYear(int year){
+    public boolean isLeapYear(int year){
         if (year % 4 == 0){
             if (year % 100 != 0){
-                return 29;
+                return true;
             }
             if (year % 400 == 0){
-                return 29;
+                return true;
             }
         }
-        return 28;
+        return false;
+    }
+
+    public int getTotalDays(int year, int month){
+        int totalDays = 0;
+        for (int i = 1; i <= year; i++){
+            if (i == year){
+                for (int m = 1; m < month; m++){
+                    totalDays += getDays(year, m);
+                }
+                break;
+            }
+            if (isLeapYear(year)){
+                totalDays += 366;
+            }else{
+                totalDays += 365;
+            }
+        }
+        return totalDays;
     }
 
     public int getDays(int year, int month){
@@ -22,17 +44,25 @@ public class Calendar {
             if (month != 2){
                 return 30;
             }else {
-                return isLeapYear(year);
+                if (isLeapYear(year)){
+                    return 29;
+                }else {
+                    return 28;
+                }
             }
         }
     }
 
-    public void print(int days){
+    public void print(int days, String weekday){
+        int weekday_num = Arrays.asList(WEEKDAYS).indexOf(weekday);
         System.out.printf("%2s %2s %2s %2s %2s %2s %2s\n","일","월","화","수","목","금","토");
         System.out.println("--------------------");
+        for (int j = 1; j <= weekday_num; j++){
+            System.out.printf("%2s ", "");
+        }
         for (int i = 1; i <= days; i++){
             System.out.printf("%2d ",i);
-            if (i % 7 == 0){
+            if (i % 7 == 7-weekday_num){
                 System.out.println();
             }
         }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,16 +10,22 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Calendar calendar = new Calendar();
         while(true){
-            System.out.println("연도와 월을 입력하세요.");
-            String input = scanner.nextLine();
-            if (input == "-1"){
+            System.out.println("연도를 입력하세요.(-1 입력시 종료)");
+            System.out.print("YEAR> ");
+            int year = Integer.parseInt(scanner.next());
+            if (year == -1){
                 System.out.println("Have a nice day!");
                 break;
             }
-            String[] year_month = input.split(" ");
-            int year = Integer.parseInt(year_month[0]);
-            int month = Integer.parseInt(year_month[1]);
-            calendar.print(calendar.getDays(year, month));
+            System.out.println("달를 입력하세요.");
+            System.out.print("MONTH> ");
+            int month = Integer.parseInt(scanner.next());
+
+            System.out.println("첫번째 요일을 입력하세요.(SU, MO, TU, WE, TH, FR, SA)");
+            System.out.print("WEEKDAY> ");
+            String weekday = scanner.next();
+            String buffer = scanner.nextLine();
+            calendar.print(calendar.getDays(year, month), weekday);
         }
         scanner.close();
     }
